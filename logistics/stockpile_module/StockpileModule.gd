428539610b5ff11e49682a6471fdbc5a2e10ba04
billbobphil <37590467+billbobphil@users.noreply.github.com>
2024-04-29 17:41:36 -0700

    Added some more scaffolding for the stockpile interface

---
+++ logistics/stockpile_module/StockpileModule.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+@onready var canvas : CanvasLayer = $CanvasLayer;
+@onready var foodAndWaterLabel : Label = $CanvasLayer/FoodAndWaterValue;
+@onready var fuelLabel : Label = $CanvasLayer/FuelValue;
+@onready var buildingMaterialsLabel : Label = $CanvasLayer/BuildingMaterialsValue;
+@onready var entertainmentLabel : Label = $CanvasLayer/EntertainmentValue;
+@onready var technologyLabel : Label = $CanvasLayer/TechnologyValue;
+@onready var interactable : Interactable = $Interactable;
+
+func _ready():
+	canvas.hide();
+	assignValuesToInterface();
+
+func assignValuesToInterface():
+	foodAndWaterLabel.text = str(ResourceManager.foodAndWater);
+	fuelLabel.text = str(ResourceManager.fuelMaterials);
+	buildingMaterialsLabel.text = str(ResourceManager.buildingMaterials);
+	entertainmentLabel.text = str(ResourceManager.entertainmentMaterials);
+	technologyLabel.text = str(ResourceManager.technologyMaterials);
+
+func _on_button_pressed():
+	canvas.hide();
+	interactable.reenableInteraction();
+
+func _on_interactable_player_exited_interactable():
+	canvas.hide();
+
+func _on_interactable_player_interacted():
+	assignValuesToInterface();
+	canvas.show();
